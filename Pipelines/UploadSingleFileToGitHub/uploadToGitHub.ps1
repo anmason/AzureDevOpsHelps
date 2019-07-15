037f9065f1df46958efade1d81ccc5302f591e5f
anmason <40646036+anmason@users.noreply.github.com>
2019-07-15 14:52:15 -0500

    Update uploadToGitHub.ps1

--- Pipelines/UploadSingleFileToGitHub/uploadToGitHub.ps1
+++ Pipelines/UploadSingleFileToGitHub/uploadToGitHub.ps1
@@ -13,24 +13,24 @@ $date = Get-Date
 # - git ls-remote will return "Repository not found" if the repo does not exist
 $REPOFOUND = git ls-remote $cloneURL | findstr "Repository not found"
 IF ([string]::IsNullOrEmpty($REPOFOUND)) {
-	# repo does exist, so clone the repo
-	cd D:\
-	git config --global user.email $userEmail
-	git config --global user.name $userDisplayName
-	git clone $cloneURL
+    # repo does exist, so clone the repo
+    cd D:\
+    git config --global user.email $userEmail
+    git config --global user.name $userDisplayName
+    git clone $cloneURL
 
     # copy the updated file to the cloned repo
-	copy $pathToFile D:\$repoName
+    copy $pathToFile D:\$repoName
 
     # push the file to the remote repo on GitHub
-	cd D:\$repoName
-	git add .
-	git commit -m "push from Azure DevOps ($date)"
-	git push
+    cd D:\$repoName
+    git add .
+    git commit -m "push from Azure DevOps ($date)"
+    git push
 }
 ELSE {
-	# Repository does not exist, inform user
-	Write-Host "Could not find repository."
+    # Repository does not exist, inform user
+    Write-Host "Could not find repository."
     Write-Host "If you have created a repository already, please check the provided URL."
     Write-Host "If you have not created a repository on GitHub yet, please do so."
 }
